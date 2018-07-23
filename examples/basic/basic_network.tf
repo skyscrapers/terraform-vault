3c0289c2d5926e31602ce322921742257bc211b1
iuri aranda <iuri.aranda@gmail.com>
2018-07-23 18:24:30 +0200

    Setup nat gateway in test

--- examples/basic/basic_network.tf
+++ examples/basic/basic_network.tf
@@ -5,3 +5,9 @@ module "vpc" {
   project                           = "${var.project}"
   amount_private_management_subnets = 2
 }
+
+module "nat_gateway" {
+  source               = "github.com/skyscrapers/terraform-network//nat_gateway?ref=3.4.1"
+  private_route_tables = "${module.vpc.private_rts}"
+  public_subnets       = "${module.vpc.public_nat-bastion}"
+}
