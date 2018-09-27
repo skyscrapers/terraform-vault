7f9baf4f2c4cc7bb8af8e02ae39df12d4c5d7544
iuri aranda <iuri.aranda@gmail.com>
2018-09-27 17:13:24 +0200

    Update teleport module version

--- vault/cloud-config.tf
+++ vault/cloud-config.tf
@@ -83,7 +83,7 @@ data "template_file" "install" {
 }
 
 module "teleport_vault1" {
-  source      = "github.com/skyscrapers/terraform-teleport//teleport-bootstrap-script?ref=2.2.1"
+  source      = "github.com/skyscrapers/terraform-teleport//teleport-bootstrap-script?ref=3.2.3"
   auth_server = "${var.teleport_auth_server}"
   auth_token  = "${var.teleport_token_1}"
   function    = "vault1"
@@ -91,7 +91,7 @@ module "teleport_vault1" {
 }
 
 module "teleport_vault2" {
-  source      = "github.com/skyscrapers/terraform-teleport//teleport-bootstrap-script?ref=2.2.1"
+  source      = "github.com/skyscrapers/terraform-teleport//teleport-bootstrap-script?ref=3.2.3"
   auth_server = "${var.teleport_auth_server}"
   auth_token  = "${var.teleport_token_2}"
   function    = "vault2"
