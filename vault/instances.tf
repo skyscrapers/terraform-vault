71f82349888c1f728d9efdfcd23cedb277ab1ab1
iuri aranda <iuri.aranda@gmail.com>
2019-01-10 12:56:40 +0100

    Update the instance modules to the latest version

--- vault/instances.tf
+++ vault/instances.tf
@@ -3,7 +3,7 @@ locals {
 }
 
 module "vault1" {
-  source        = "github.com/skyscrapers/terraform-instances//instance?ref=2.3.2"
+  source        = "github.com/skyscrapers/terraform-instances//instance?ref=2.3.6"
   project       = "${var.project}"
   environment   = "${var.environment}"
   name          = "vault1"
@@ -16,7 +16,7 @@ module "vault1" {
 }
 
 module "vault2" {
-  source        = "github.com/skyscrapers/terraform-instances//instance?ref=2.3.2"
+  source        = "github.com/skyscrapers/terraform-instances//instance?ref=2.3.6"
   project       = "${var.project}"
   environment   = "${var.environment}"
   name          = "vault2"
